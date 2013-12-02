16a367cb6ff7d6b5754966b3d1ddc3843dff707b
happenstance <happenstance9@yahoo.com>
2013-12-02 12:32:17 -0500

    initial commit

---
+++ app/js/main.js
@@ -0,0 +1,13 @@
+$(document).foundation({
+  orbit: {
+    animation: 'slide',
+    timer_speed: 9000,
+    resume_on_mouseout: true,
+    animation_speed: 1200,
+    navigation_arrows: true,
+    bullets: true,
+    slide_number: false,
+  }
+});
+
+$(document).foundation();
